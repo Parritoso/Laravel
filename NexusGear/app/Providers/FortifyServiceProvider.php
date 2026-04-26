6088300765f59e923bf84bde341dfcf237a85e08
Parritoso <jsanchezclaro@gmail.com>
2026-04-26 13:55:56 +0200

    Añadido las dependencias Boostrap & Sass #6 - Tambien se modifico el .gitignore para que no se incluyan los archivos de configuración de vscode

--- NexusGear/app/Providers/FortifyServiceProvider.php
+++ NexusGear/app/Providers/FortifyServiceProvider.php
@@ -34,6 +34,12 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
         Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
+        Fortify::loginView(function(){
+            return view('auth.login');
+        });
+        Fortify::registerView(function(){
+            return view('auth.register');
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
