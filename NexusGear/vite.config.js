10f44893c697ce9b98f23c7809fc5ec2f9d4fd73
Alonso JF <alonsojf17@gmail.com>
2026-05-10 19:22:05 +0200

    fix: incluir todos los assets de Vite en el manifest

--- NexusGear/vite.config.js
+++ NexusGear/vite.config.js
@@ -5,7 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css','resources/css/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/app.scss',
+                'resources/css/admin.scss',
+                'resources/css/checkout.scss',
+                'resources/css/home.scss',
+                'resources/css/login.scss',
+                'resources/css/onboarding.scss',
+                'resources/css/password-view.scss',
+                'resources/js/app.js',
+                'resources/js/checkout.js',
+                'resources/js/discounts_form.js',
+                'resources/js/login.js',
+                'resources/js/onboarding.js',
+                'resources/js/password-view.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
