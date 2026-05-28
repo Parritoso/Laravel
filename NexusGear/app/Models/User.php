6db72a879eac207e6335b9e03fbf87f9c91ac493
Daniel Valladolid <danielvalladolidmoreno@gmail.com>
2026-05-28 20:58:27 +0200

    Arreglo 2Fa #57

--- NexusGear/app/Models/User.php
+++ NexusGear/app/Models/User.php
@@ -10,12 +10,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
     protected $table = 'users';
 
     protected $fillable = [
