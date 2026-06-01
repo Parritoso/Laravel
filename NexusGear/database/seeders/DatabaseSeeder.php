5c5e856460101b35cb96cdfa1867afd645ce3592
Alonso JF <alonsojf17@gmail.com>
2026-06-01 15:55:06 +0200

    feat(deploy): añade datos de demostración para el VPS (#62)

--- NexusGear/database/seeders/DatabaseSeeder.php
+++ NexusGear/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             ProductoCategoriaSeeder::class,
             UserSeeder::class,
+            DemoDataSeeder::class,
         ]);
     }
 }
